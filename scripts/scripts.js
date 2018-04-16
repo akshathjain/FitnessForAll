caa8bab7fc7641363ea8d0a8a00c0ea1756e4532
akshathjain <akshath.r.jain@gmail.com>
2018-04-16 08:02:01 -0400

    changed up navbar colors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,24 +15,39 @@ $(document).ready(function(){
 
     //function to check to see if hamburger menu button clicked
     document.getElementById("navbarCollapseButton").addEventListener("click", function(){
-        menuClicked = !menuClicked;
-        checkPosition(menuClicked);
+    	menuClicked = !menuClicked;
+    	checkPosition(menuClicked);
     });
 
 	//function that checks the page position and edits how the navbar looks based on where the user has scrolled
 	function checkPosition(menuStatus){
 		var position = $(this).scrollTop();
+		var list = document.getElementsByClassName("nav-link-id");
 		if(position == 0){
-            if(!menuStatus)
-                $(".navbar").removeClass("navbarStateWhenScrolled");
-            else
-                $(".navbar").addClass("navbarStateWhenScrolled");
-        }else
+			if(!menuStatus){
+				$(".navbar").removeClass("navbarStateWhenScrolled");
+				document.getElementById("nav-header-id").style.color = "#ffffff";
+				for (var i = list.length - 1; i >= 0; i--) {
+					list[i].style.color = "#ffffff";
+				}
+			}else{
+				$(".navbar").addClass("navbarStateWhenScrolled");
+				document.getElementById("nav-header-id").style.color = "#01345B";
+				for (var i = list.length - 1; i >= 0; i--) {
+					list[i].style.color = "#01345B";
+				}
+			}
+		}else{
 			$(".navbar").addClass("navbarStateWhenScrolled");
+			document.getElementById("nav-header-id").style.color = "#01345B";
+			for (var i = list.length - 1; i >= 0; i--) {
+				list[i].style.color = "#01345B";
+			}
+		}
 	}
 
 	// Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+	$("a").on('click', function(event) {
 
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
@@ -46,8 +61,8 @@ $(document).ready(function(){
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
-                }, 800, function(){
+            	scrollTop: $(hash).offset().top
+            }, 800, function(){
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
